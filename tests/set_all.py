18149225e10377ad913e65f20212a4fa0f212f55
Zayne Marsh <saynemarsh9@gmail.com>
2024-04-07 09:05:15 -0400

    Fixed test script.

--- tests/set_all.py
+++ tests/set_all.py
@@ -5,7 +5,8 @@ a = n.Neobridge(serial.Serial(
             'COM3',
             baudrate=115200,
             timeout=0.05,
-            write_timeout=1))
+            write_timeout=1), 30)
 
 for i in range(255):
     a.setall((i, i, i))
+    a.show()
